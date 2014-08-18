464a2a9180a6a5e77db24d7646ce5fe4020732e1
TheDarkVoid <guyana393@gmail.com>
2014-08-18 17:26:53 -0400

    Initial Testing

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+	public float width;
+	public float startPos;
+	public GameObject ground;
+
+	public bool reGen = false;
+
+	void Start () 
+	{
+		for(int i = 0; i < 64; i++)
+		{
+			GameObject obj = Instantiate(ground, new Vector3((i * width)+startPos, 0), Quaternion.identity) as GameObject;
+			obj.GetComponent<SpriteRenderer>();
+		}
+	}
+
+	void Update()
+	{
+		if(!reGen)
+			return;
+		reGen = false;
+		Start();
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public float accel;
+	public float jumpSpeed;
+
+	private Vector2 thisTransform;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		thisTransform = new Vector2(transform.position.x, transform.position.y);
+		if(rigidbody2D.velocity.x > speed)
+		{
+			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+		}else
+			rigidbody2D.AddForce(new Vector2(accel,0));
+
+		Debug.DrawLine(transform.position, new Ray(transform.position, transform.up*-1).GetPoint(.6f), Color.red);
+		if(Physics2D.Raycast(thisTransform, new Vector2(0,-1), .6f).collider != collider)
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				rigidbody2D.AddForce(new Vector2(0,jumpSpeed));
+			}
+		}
+
+	}
+}
